9a95e7a4d9516f237369842fa3a0b096664e4e53
Aaron Fleming <ercreeper@gmail.com>
2022-03-04 14:07:11 -0800

    Added tests for sort.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from sort import sort_list
+
 def main():
-    print("Hello World!")
+    test_p1()
+
+def test_p1():
+    print("----- Problem 1 -----")
+    print(sort_list([1, 3, 2, 7]))
+    print(sort_list([3, 2, 4, 89]))
+    print(sort_list([]))
+    print(sort_list([1]))
+    print(sort_list(['a', 'd', 'c', 'b']))
+    print("---------------------")
 
 if __name__ == '__main__':
     main()
